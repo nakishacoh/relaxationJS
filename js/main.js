c000568c28215a0fb24be328880a8ccbc8f5f41b
nakishaco <nakishacoh@gmail.com>
2019-04-01 20:34:28 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var calmSongs = [
+  {song: "velvetlight.mp3"}, {song: "boywithacoin.mp3"}
+]
+
+$(document).ready(function(){
+  $("calmBtn").click(function () {
+    e.preventDefault();
+
+    var target = e.target;
+
+    var audio = $("#calm");
+    var source = $("#source");
+
+    console.log(audio);
+
+
+  })
+})
